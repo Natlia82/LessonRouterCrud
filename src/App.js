2926a0a61d8df99682c67ce5e7511ab545a3c8a4
Natalia <nata@host>
2023-05-01 19:16:52 +0500

    убрала лишнее

--- src/App.js
+++ src/App.js
@@ -3,25 +3,32 @@ import React from 'react';
 import { Routes, Route, NavLink, Navigate, useSearchParams } from "react-router-dom";
 import HomePage from './components/HomePage';
 import NewPost from './components/NewPost';
-import ForzaPage from './components/ForzaPage';
 import ViewingPost from './components/ViewingPost';
-import Menu from './components/Menu';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div>
-     
-       <Routes>
-           <Route path='/' element={<HomePage />} />
-           <Route path='/posts/new' element={<NewPost />} />
-           <Route path='/posts/:id' element={<ViewingPost />} />
-           <Route path='/forza' element={<ForzaPage />} />
-           
-       </Routes>
-     
-    </div>
-  );
+    return ( <
+        div >
+
+        <
+        Routes >
+        <
+        Route path = '/'
+        element = { < HomePage / > }
+        /> <
+        Route path = '/posts/new'
+        element = { < NewPost / > }
+        /> <
+        Route path = '/posts/:id'
+        element = { < ViewingPost / > }
+        />
+
+        <
+        /Routes>
+
+        <
+        /div>
+    );
 }
 
 export default App;
